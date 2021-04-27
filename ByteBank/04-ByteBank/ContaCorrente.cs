3c2d163e46e5c1c52f139673c0b1777e2fa7cc39
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-04-26 22:42:58 -0300

    Aula 3 finalizada

---
+++ ByteBank/04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    public class ContaCorrente
+    {
+        public string titular { get; set; }
+        public int agencia { get; set; }
+        public int numero { get; set; }
+        public double saldo { get; set; } = 100;
+
+        public bool Sacar (double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+    }
+}
