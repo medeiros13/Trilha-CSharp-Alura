90e151933ca7813a3d80ebf9deba5523eb3e194d
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-01 15:16:04 -0300

    Solução mal feita utilizando IF e atributos privados

---
+++ HerancasEInterfaces/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        //0 = funcionário
+        //1 = diretor
+        //2 = designer
+        //3 = gerente de conta corrente
+        //4 = coordenador
+        //n = ...
+        private int _tipo;
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+        //Cria-se um método ao invés de uma propriedade para retornar um valor, pois o uso de propriedades geralmente é utilizado quando se tem um acesso imediato ao valor, sem precisar executar algum cálculo, ou alguma ação que demande um custo de processamento
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+            {
+                return Salario;
+            }
+            else
+            {
+                return Salario * 0.10;
+            }
+        }
+    }
+}
---
+++ HerancasEInterfaces/ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "033.437.160-06";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
