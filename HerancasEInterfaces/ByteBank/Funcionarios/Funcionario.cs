2c6cebe3795f8aa36b4a91141235e4ab61409556
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-01 22:12:21 -0300

    Solução melhorada com a alteração dos métodos GetBonificacao e AumentarSalario de concretos para abstratos, com isso, sempre que o programador for criar novas classes que herdem da classe Funcionario, será obrigatório sobrescrever os métodos citados, caso contrário o código não irá compilar.

--- HerancasEInterfaces/ByteBank/Funcionarios/Funcionario.cs
+++ HerancasEInterfaces/ByteBank/Funcionarios/Funcionario.cs
@@ -22,17 +22,10 @@ namespace ByteBank.Funcionarios
             TotalDeFuncionarios++;
         }
 
-        public virtual void AumentarSalario()
-        {
-            Salario *= 1.1; //Aumenta 10%
-        }
-
-        //Cria-se um método ao invés de uma propriedade para retornar um valor, pois o uso de propriedades geralmente é utilizado quando se tem um acesso imediato ao valor, sem precisar executar algum cálculo, ou alguma ação que demande um custo de processamento
-        //O uso da palavra "virtual" significa que esse método pode ser sobrescrito nas suas classes filhas
-        //Ou seja, o método GetBonificacao do Funcionario pode ser sobrescrito pelo Diretor, Designer, Coordenador, e outros diversos tipos de Funcionarios
-        public virtual double GetBonificacao()
-        {
-            return Salario * 0.10;
-        }
+        //Os métodos foram alterados para abstratos, para que quando formos criar uma nova classe que herde de Funcionário,
+        //seja obrigatório sobrescrever esses 2 métodos.
+        //métodos abstratos só podem fazer parte classes abstratas. Classes concretas só implementam métodos concretos.
+        public abstract void AumentarSalario();
+        public abstract double GetBonificacao();
     }
 }
