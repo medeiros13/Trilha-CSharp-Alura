c4e4357c502bc9ef1fe9419bc723209680ee89c8
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-05 19:29:51 -0300

    Solução melhorada adicionando propriedades readonly para o número da conta e da agência da classe ContaCorrente, impedindo que esses campos possam ter seus valores alterados fora do construtor da classe.

---
+++ Excecoes/ByteBank/ContaCorrente.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public Cliente Titular { get; set; }
+
+        //Atributos readonly não tem setters em suas propriedades, e só podem ter seu valor setado no construtor da classe, fora dele, isso não é possível
+        //Isso é útil para informações que só vão receber valor uma única vez
+        public int Numero { get; } //Se você só deixar o get na propriedade, o compilador entende que ela é readonly, só podendo receber valor no construtor da classe
+        public int Agencia { get; } //Mais uma propriedade readonly
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+
+            TotalDeContasCriadas++;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
