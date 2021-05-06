77406c7b8a63e8c2c313ed647369855374fc2e9f
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-05 21:06:09 -0300

    Aprimorada a solução com a criação de exceções personalizadas

--- Excecoes/ByteBank/Program.cs
+++ Excecoes/ByteBank/Program.cs
@@ -12,56 +12,33 @@ namespace ByteBank
         {
             try
             {
-                ContaCorrente conta = new ContaCorrente(514, 0);
-                Metodo();
+                ContaCorrente conta = new ContaCorrente(514, 5435);
+                conta.Depositar(50);
+                Console.WriteLine(conta.Saldo);
+                conta.Sacar(500);
+                //Metodo();
+            }
+            catch (SaldoInsuficienteException e)
+            {
+                Console.WriteLine(e.Message);
             }
             catch (ArgumentException e)
             {
-                Console.WriteLine($"Argumento com o problema: {e.ParamName}");
+                Console.WriteLine($"Argumento com o problema: {e.ParamName}.");
                 Console.WriteLine(e.Message);
             }
             catch (DivideByZeroException e)
             {
-                Console.WriteLine("Ocorreu uma exceção do tipo DivideByZeroException");
+                Console.WriteLine("Ocorreu uma exceção do tipo DivideByZeroException.");
                 Console.WriteLine(e.Message);
             }
             catch (NullReferenceException e)
             {
-                Console.WriteLine("Aconteceu um erro!");
+                Console.WriteLine("Aconteceu um erro.");
                 Console.WriteLine(e.Message);
             }
 
             Console.ReadLine();
-        }
-
-        // Teste com a cadeia de chamada:
-        // Metodo -> TestaDivisao -> Dividir
-        private static void Metodo()
-        {
-            TestaDivisao(0);
-        }
-
-        private static void TestaDivisao(int divisor)
-        {
-            try
-            {
-                int resultado = Dividir(10, divisor);
-                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
-            }
-            catch (DivideByZeroException erro)
-            {
-                Console.WriteLine(erro.Message);
-                Console.WriteLine(erro.StackTrace);
-                Console.WriteLine("Não é possível fazer uma divisão por 0!");
-            }
-        }
-
-        private static int Dividir(int numero, int divisor)
-        {
-            ContaCorrente conta = null;
-            Console.WriteLine(conta.Saldo);
-
-            return numero / divisor;
         }
     }
 }
