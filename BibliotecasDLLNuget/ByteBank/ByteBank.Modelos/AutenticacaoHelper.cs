a1c5d9514546d1236a0141512c0e07f865d79803
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-08 16:18:19 -0300

    Solução aprimorada movendo tosas as classes das Entidades do sistema para dentro de uma biblioteca chamada ByteBank.Modelos, com isso é possível reutilizar essa biblioteca modelos para vários projetos diferentes.

---
+++ BibliotecasDLLNuget/ByteBank/ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    //O modificador "internal" diz pro compilador que essa classe só é acessível aqui dentro dessa biblioteca,
+    //e projetos externos que utilizam essa biblioteca, não possuírão acesso a classe AutenticacaoHelper
+    internal class AutenticacaoHelper
+    {
+        public bool CompararSenhas(string senhaVerdadeira, string senhaTentativa)
+        {
+            return senhaVerdadeira == senhaTentativa;
+        }
+    }
+}
