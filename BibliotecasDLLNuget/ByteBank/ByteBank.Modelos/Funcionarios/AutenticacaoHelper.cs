1dc6f121c564290a6a0066ea571fb9bb80aabadd
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-11 19:34:14 -0300

    Criado arquivo xml de documentação da biblioteca, para que esse arquivo de documentação seja lido pelo Visual Studio, é necessário que esteja marcada a opção XML documentation file nas propriedades de debug do projeto da biblioteca

---
+++ BibliotecasDLLNuget/ByteBank/ByteBank.Modelos/Funcionarios/AutenticacaoHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ByteBank.Modelos.Funcionarios
+{
+    internal class AutenticacaoHelper
+    {
+        internal bool CompararSenhas(string senha1, string senha2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BibliotecasDLLNuget/ByteBank/ByteBank.Modelos/ParceiroComercial.cs
+++ BibliotecasDLLNuget/ByteBank/ByteBank.Modelos/ParceiroComercial.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Modelos.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
