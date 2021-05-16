69ecb67f441a79c10d5662da3c8dab0d7bdd41b9
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-15 21:27:23 -0300

    Solução melhorada utilizando o método ToString para representar objetos como strings

--- StringsRegexEObject/ByteBank.Modelos/Cliente.cs
+++ StringsRegexEObject/ByteBank.Modelos/Cliente.cs
@@ -12,8 +12,14 @@ namespace ByteBank.Modelos
         public string CPF { get; set; }
         public string Profissao { get; set; }
 
+
         //O método Equals serve para mostrar se o objeto passado por parâmetro é igual ao objeto dessa classe, para isso, é necessário definir o que faz eles serem iguais
         //No caso desse Equals, os clientes vão ser iguais se tiverem o mesmo CPF, que é o identificador (ID) da classe
+        /// <summary>
+        /// Compara se o objeto passado por parâmetro contém o mesmo CPF que o cliente deste objeto
+        /// </summary>
+        /// <param name="obj">objeto para comparar</param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
             //Cliente outroCliente = (Cliente)obj; //Essa conversão tenta converter, se não conseguir lança uma InvalidCastException
@@ -25,6 +31,16 @@ namespace ByteBank.Modelos
             }
 
             return CPF == outroCliente.CPF;
+        }
+
+        //O método ToString serve para representar o nosso objeto como uma string, é muito utilizado em Console.WriteLines e outros métodos de saída de dados
+        /// <summary>
+        /// Representação de String do Cliente
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"CPF: {CPF}, Nome: {Nome}, Profissão: {Profissao}";
         }
     }
 }
