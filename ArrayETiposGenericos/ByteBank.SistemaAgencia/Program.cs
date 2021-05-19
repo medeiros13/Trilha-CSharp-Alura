e9d10a912bb4a513d7cd6897c32be139e3f8d850
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-18 21:18:42 -0300

    Solução melhorada utilizando o array.length

--- ArrayETiposGenericos/ByteBank.SistemaAgencia/Program.cs
+++ ArrayETiposGenericos/ByteBank.SistemaAgencia/Program.cs
@@ -14,30 +14,25 @@ namespace ByteBank.SistemaAgencia
         {
 
             //Array de inteiros com 5 posições!
-            int[] idades = new int[5];
+            int[] idades = new int[3];
             idades[0] = 15;
-            idades[1] = 25;
-            idades[2] = 30;
-            idades[3] = 40;
-            //idades[4] = 45;
+            idades[1] = 28;
+            idades[2] = 35;
+            //idades[3] = 50;
+            //idades[4] = 28;
+            //idades[5] = 60;
 
-            int idadeNoIndice4 = idades[4];
-            Console.WriteLine(idadeNoIndice4); //Vai mostrar zero, pois zero é o valor padrão do int. O array, quando não especificado um valor para alguma posição, ele assume o valor padrão de seu tipo primitivo
-
-            int[] outroArray = idades;
-            Console.WriteLine(outroArray[3]);
-
-            bool[] arrayDeBooleans = new bool[10];
-            arrayDeBooleans[0] = true;
-            arrayDeBooleans[1] = false;
-            arrayDeBooleans[2] = true;
-            arrayDeBooleans[3] = false;
-            arrayDeBooleans[4] = false;
-            arrayDeBooleans[5] = true;
-            arrayDeBooleans[6] = false;
-            arrayDeBooleans[7] = false;
-            arrayDeBooleans[8] = true;
-            arrayDeBooleans[9] = false;
+            Console.WriteLine(idades.Length);
+            int acumulador = 0;
+            for (int i = 0; i < idades.Length; i++)
+            {
+                int idade = idades[i];
+                Console.WriteLine($"Acessando o array Idades no índice: {i}");
+                Console.WriteLine($"valor de idades[{i}] = {idade}");
+                acumulador += idade;
+            }
+            int media = acumulador / idades.Length;
+            Console.WriteLine($"Média de idades = {media}");
             Console.ReadLine();
         }
     }
