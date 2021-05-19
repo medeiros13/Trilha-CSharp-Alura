2197e7cf3e972f1347bc231bcde2ccf1da31785d
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-18 21:09:38 -0300

    Criado meu array de booleans

--- ArrayETiposGenericos/ByteBank.SistemaAgencia/Program.cs
+++ ArrayETiposGenericos/ByteBank.SistemaAgencia/Program.cs
@@ -19,9 +19,25 @@ namespace ByteBank.SistemaAgencia
             idades[1] = 25;
             idades[2] = 30;
             idades[3] = 40;
-            idades[4] = 45;
+            //idades[4] = 45;
 
-            Console.WriteLine(idades[4]);
+            int idadeNoIndice4 = idades[4];
+            Console.WriteLine(idadeNoIndice4); //Vai mostrar zero, pois zero é o valor padrão do int. O array, quando não especificado um valor para alguma posição, ele assume o valor padrão de seu tipo primitivo
+
+            int[] outroArray = idades;
+            Console.WriteLine(outroArray[3]);
+
+            bool[] arrayDeBooleans = new bool[10];
+            arrayDeBooleans[0] = true;
+            arrayDeBooleans[1] = false;
+            arrayDeBooleans[2] = true;
+            arrayDeBooleans[3] = false;
+            arrayDeBooleans[4] = false;
+            arrayDeBooleans[5] = true;
+            arrayDeBooleans[6] = false;
+            arrayDeBooleans[7] = false;
+            arrayDeBooleans[8] = true;
+            arrayDeBooleans[9] = false;
             Console.ReadLine();
         }
     }
