0e0e2380b7b523c63bd58b5f0883ea223b922aa6
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-18 21:02:23 -0300

    Criado o primeiro array de inteiros

---
+++ ArrayETiposGenericos/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Array de inteiros com 5 posições!
+            int[] idades = new int[5];
+            idades[0] = 15;
+            idades[1] = 25;
+            idades[2] = 30;
+            idades[3] = 40;
+            idades[4] = 45;
+
+            Console.WriteLine(idades[4]);
+            Console.ReadLine();
+        }
+    }
+}
