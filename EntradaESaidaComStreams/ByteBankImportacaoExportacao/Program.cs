629a78dfb6be1c65fe6b616ac86917496ad6995c
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-05-21 22:02:40 -0300

    Feito nosso primeiro programa para ler os bytes de um txt e armazenar em um buffer, o buffer lê 1 kb por vez, e vai sendo atualizado conforme avança na leitura do arquivo

---
+++ EntradaESaidaComStreams/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,40 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //IO = Input e Output
+
+namespace ByteBankImportacaoExportacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var enderecoDoArquivo = "contas.txt";
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024]; //1kb
+            var numeroBytesLidos = -1;
+
+            while (numeroBytesLidos != 0)
+            {
+                numeroBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+
+            }
+            EscreverBuffer(buffer);
+
+            Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+        }
+    }
+}
